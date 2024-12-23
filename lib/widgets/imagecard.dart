4d6a0f6bd8064cd10b16ea2d669463cb925bc63e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-23 14:45:13 +0100

    build: images container

---
+++ lib/widgets/imagecard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    super.key,
+    required this.image
+  });
+
+final String image;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 200,
+      child: Image.network(
+        image,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
