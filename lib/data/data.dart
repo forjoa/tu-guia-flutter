f5e9e4ec1a67598bc65301c87648bfa5f3d143fc
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-23 14:43:38 +0100

    feat: simulate db

---
+++ lib/data/data.dart
@@ -0,0 +1,57 @@
+const List<Map<String, String>> listCard = [
+  {
+    "name": "Joaquin Trujillo",
+    "description": "Hola mundo...",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Andrea López",
+    "description": "Viajera apasionada y fotógrafa.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Carlos Medina",
+    "description": "Amante de la tecnología y los videojuegos.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Lucía Pérez",
+    "description": "Cocinera y escritora amateur.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Diego Fernández",
+    "description": "Explorador urbano y coleccionista de antigüedades.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "María Torres",
+    "description": "Artista plástica y diseñadora gráfica.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Fernando Gómez",
+    "description": "Programador web y creador de contenido.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Sofía Martínez",
+    "description": "Amante de los libros y los animales.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Juan Herrera",
+    "description": "Aventurero y amante de la naturaleza.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Ana Gutiérrez",
+    "description": "Entusiasta de la música y los festivales.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  },
+  {
+    "name": "Pedro Ruiz",
+    "description": "Deportista y motivador personal.",
+    "image": "https://pixlr.com/images/generator/text-to-image.webp"
+  }
+];
